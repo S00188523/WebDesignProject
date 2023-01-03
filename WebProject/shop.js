3103d12a9b6915f82425c3cb2f2893700f34860f
adamgrace00 <118433143+adamgrace00@users.noreply.github.com>
2023-01-03 19:38:09 +0000

    Add files via upload

---
+++ WebProject/shop.js
@@ -0,0 +1,37 @@
+var addtocart = document.getElementById('addtocart');
+addtocart.addEventListener("click", addToCart);
+
+
+function addToCart() {
+    var total=localStorage.getItem('checkout');
+    total++;
+    localStorage.setItem('checkout',total);
+    document.querySelector('#checkout').innerHTML=total;
+}
+var countDownDate = new Date("Jan 10, 2023 11:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("Countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
